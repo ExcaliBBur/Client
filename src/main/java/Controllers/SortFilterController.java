ae0d0657e7f2f3a874715cf7dc2558e1f08274fb
ShipIM <ilya.shipunov@gmail.com>
2022-05-17 21:20:31 +0300

    Sasha CorrectedProgress

--- src/main/java/Controllers/SortFilterController.java
+++ src/main/java/Controllers/SortFilterController.java
@@ -1,18 +1,25 @@
 package Controllers;
 
+import Models.Command;
+import Models.Rule;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.List;
 
 public class SortFilterController {
+    private Option purpose;
+    private List<Command> rules;
 
     @FXML
-    private ChoiceBox<?> choiceBox;
+    private ChoiceBox<Rule.Order> optionBox;
 
     @FXML
-    private ComboBox<?> comboBox;
+    private ComboBox<Rule.Column> columnBox;
 
     @FXML
     private TextField textField;
@@ -21,6 +28,45 @@ public class SortFilterController {
     private Button okButton;
 
     @FXML
-    private Button buttonCancel;
+    private Button cancelButton;
 
+    @FXML
+    public void confirm() {
+        switch (purpose) {
+            case ADD: {
+
+            }
+            case EDIT: {
+
+            }
+        }
+    }
+
+    @FXML
+    public void cancel() {
+        Stage stage = (Stage) this.cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void initialize() {
+        this.columnBox.getItems().addAll(Rule.Column.values());
+        this.optionBox.getItems().addAll(Rule.Order.values());
+    }
+
+    public enum Option {
+        EDIT("EDIT"),
+        ADD("ADD");
+
+        private final String name;
+
+        Option(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
+
