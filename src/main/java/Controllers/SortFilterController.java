5ca5ae77a730be4cdc357d0e001a95f5c00fb779
Alexander <excalibburn@shinra-tensei.ru>
2022-05-17 18:48:06 +0300

    SortFilter
    SortFilter

---
+++ src/main/java/Controllers/SortFilterController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SortFilterController {
+
+    @FXML
+    private ChoiceBox<?> choiceBox;
+
+    @FXML
+    private ComboBox<?> comboBox;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private Button buttonCancel;
+
+}
